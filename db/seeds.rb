1e42d56354bad4811e479f0dd2e1b549ae262a1a
HannaBM12 <Hannabmulugeta@gmail.com>
2021-02-06 23:29:35 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SchoolClass.destroy_all
+Student.destroy_all
+
+
+
+sc1 = SchoolClass.create(title: 'Home Room', room_number: '101')
+sc2 = SchoolClass.create(title: 'Math', room_number: '102')
+sc3 = SchoolClass.create(title: 'Software Eng', room_number: '102')
+sc4 = SchoolClass.create(title: 'Data Science', room_number: '103')
+sc5 = SchoolClass.create(title: 'Cyber Security', room_number: '104')
+
+
+s1 = Student.create(first_name: 'Hanna', last_name: 'Mulugeta')
+s2 = Student.create(first_name: 'Hella', last_name: 'Alemayehu')
+s3 = Student.create(first_name: 'Ruth', last_name: 'Marie')
+s4 = Student.create(first_name: 'Elizabeth', last_name: 'Mesfin')
+s5 = Student.create(first_name: 'Samuel', last_name: 'Teressa')
